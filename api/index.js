6d41f9ad87fd428c7fd21b44a5378b5763e0bb76
biplove subedi <subedi.biplove10@gmail.com>
2024-04-30 15:59:53 +0545

    sign up api created

--- api/index.js
+++ api/index.js
@@ -2,18 +2,26 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routers/user.route.js";
+import authRoutes from "./routers/auth.route.js";
+
 dotenv.config();
+
+const app = express();
+
+
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDb is connected");
   })
   .catch((error) => console.log(error));
-
-const app = express();
-
-app.listen("3000", () => {
+ 
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
