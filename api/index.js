a686af8b1ede5e8727f1d5161e468415900124cd
biplove subedi <subedi.biplove10@gmail.com>
2024-04-30 13:41:49 +0545

    mongodb configure

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 
-app.listen('3000' ,()=>
-{
-    console.log('Server is running on port 3000')
-})
+app.listen("3000", () => {
+  console.log("Server is running on port 3000");
+});
