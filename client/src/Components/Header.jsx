b2ea4e8df90673c6d8c31db592eb718644a0dda0
biplove subedi <subedi.biplove10@gmail.com>
2024-04-30 13:10:54 +0545

    create and run server using express

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -34,11 +34,11 @@ const Header = () => {
           <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2">
-          <Button className=" w-10 h-10 lg:hidden md:hidden " color="gray" pill>
+          <Button className=" w-10 h-10 flex items-center justify-center" color="gray" pill>
             <FaMoon />
           </Button>
           <Link to="/sign-in">
-            <Button gradientDuoTone='purpleToBlue'>
+            <Button gradientDuoTone='purpleToBlue' outline>
               SignIn
             </Button>
           </Link>
