9b246a4b16b6ebae8ea89a13436a295c9690e3e6
biplove subedi <subedi.biplove10@gmail.com>
2024-05-02 16:50:01 +0545

    Dashboard made

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import DashSidebar from "../Components/DashSidebar.jsx";
+import DashProfile from "../Components/DashProfile.jsx";
 
 const Dashboard = () => {
   const location = useLocation()
+const [tab,setTab]=useState()
+useEffect(()=>
+{
+  const urlParams = new URLSearchParams(location.search)
+  console.log(urlParams)
+  const tabFromUrl = urlParams.get('tab')
+  if(tabFromUrl)
+  {
+    setTab(tabFromUrl)
+  }
+},[location.search])
 
   return (
-    <div>
-    
+    <div className="min-h-screen flex flex-col md:flex-row ">
+<div  className='md:w-56'>
+<DashSidebar/>
+</div>
+{tab === 'profile' && <DashProfile/>}
     </div>
   )
 }
