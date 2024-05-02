5b000258e5301bb2eb77f496b50f740428fca96b
biplove subedi <subedi.biplove10@gmail.com>
2024-05-02 12:35:43 +0545

    redux persist used for storage of information in browser

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,24 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "./user/userSlice.js";
+import { combineReducers } from "@reduxjs/toolkit";
+import {persistReducer} from 'redux-persist'
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+    user:UserReducer
+})
+const persistConfig = {
+    key: 'root',
+    storage,
+    version : 1,
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
+  reducer: persistedReducer,
+  middleware : (getDefaultMiddleware) =>
+  getDefaultMiddleware({serializableCheck:false})
 });
+
+export const persistor = persistStore(store)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./app/store.js";
+import { store,persistor } from "./app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+ < PersistGate persistor={persistor}>
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>
-);
+</PersistGate>
+)
+
