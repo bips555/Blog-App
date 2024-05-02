b2bb8e165f75105f585d04ef13716d61251ab8ad
biplove subedi <subedi.biplove10@gmail.com>
2024-05-02 15:47:01 +0545

    fixed return

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
+  const location = useLocation()
+
   return (
     <div>
-      Dashboard
+    
     </div>
   )
 }
