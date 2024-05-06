650feb2df89813db36867c9678f7694853a19ec2
biplove subedi <subedi.biplove10@gmail.com>
2024-05-06 13:52:29 +0545

    Call to Action added

---
+++ client/src/Components/CalltoAction.jsx
@@ -0,0 +1,25 @@
+import { Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Want to learn more about Me?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Checkout these Projects I have made using various javascript frameoworks and libraries
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <Link to="/projects">
+                    Projects
+                </Link>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        </div>
+    </div>
+  )
+}
